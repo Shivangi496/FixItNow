81d992b5c397c9fdab440ce4928f9e181c60051b
Munazza <munazzanasir441@gmail.com>
2025-02-24 22:47:38 +0530

    Adding p tag

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ function App() {
       <div>
       <h1>Hello Guys</h1>
       <h1>Hi, how are you</h1>
+      <p>This is the advance project based on different type of services provided to the customers.</p>
       </div>
       
      
