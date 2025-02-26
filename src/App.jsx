72271e87d6be0f9dd29046e113c5ee62ec6628a5
Shivangi496 <133697302+Shivangi496@users.noreply.github.com>
2025-02-25 21:53:21 -0800

    add style in h1 tag

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,10 @@ function App() {
   return (
     <>
       <div>
-      <h1>Hello Guys</h1>
+      <h1  style={{color:"red"}}>Hello Guys</h1>
       <h1>Hi, how are you</h1>
       <p>This is the advance project based on different type of services provided to the customers.</p>
+      
       </div>
       
      
